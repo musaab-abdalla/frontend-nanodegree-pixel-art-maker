08a8bc7d6d161a66b2bb17e17da5da62cc9f02ac
Musaab Abdalla <amazonfiretv.test@gmail.com>
2017-09-06 23:28:06 -0400

    docs: changes to documentation

---
+++ README.md
@@ -0,0 +1,43 @@
+# Pixel Art Maker
+
+##### Overview
+
+Udacity - Front-End Web Developer Nanodegree project. In this project were given starter code, including HTML and CSS, to build the application. we'll write JavaScript code that lets the user to create a grid of squares representing their design, and apply colors to those squares to create a digital masterpiece!. were build a single-page web app that allows users to draw pixel art on a customizable canvas! Check out this example:
+
+## Getting Started
+
+##### Live
+**Go to** https://musaab-abdalla.github.io/frontend-nanodegree-pixel-art-maker
+
+##### Locally
+
+**1.** Clone this repo:
+
+Clone the repo using Git:
+
+```
+git clone https://github.com/musaab-abdalla/frontend-nanodegree-pixel-art-maker.git
+```
+
+**2.** Open the application:
+
+Users should be able to:
+
+1. Dynamically set the size of the table as an H by W grid.
+2. Choose a color.
+3. Click a cell in the grid to fill that cell with the chosen color.
+
+```
+$ open "http://localhost/frontend-nanodegree-pixel-art-maker/index.html"
+```
+
+Dynamically creates a grid that you can interact with.
+
+## Skills
+
+##### Overview
+
+**1.** Defining variables with `const` and `let`.<br />
+**2.** Accessing the DOM using methods of the `document` object.<br />
+**3.** Declaring functions and attaching them to DOM objects as event listeners.<br />
+**4.** Writing nested loops and using loop variables.<br />
---
+++ designs.js
@@ -0,0 +1,41 @@
+// add event listener When form gets submitted and call makeGrid() 
+document.querySelector("#sizePicker").addEventListener("submit", function (e) {
+    e.preventDefault();
+    makeGrid();
+}, false);
+
+function makeGrid() {
+    // Define user input height and width.
+    const pixelHeight = document.querySelector("#input_height").value;
+    const pixelWidth = document.querySelector("#input_width").value;
+
+    // Define table element.
+    const pixelTable = document.querySelector("#pixel_canvas");
+
+    // Clear the table when user re-submit height and width.
+    // https://stackoverflow.com/questions/7271490/delete-all-rows-in-an-html-table#answer-44971572
+    while (pixelTable.hasChildNodes()) {
+        pixelTable.removeChild(pixelTable.lastChild);
+    }
+
+    // Loops dynamically to create the table based on user input.
+    for (let row = 0; row < pixelHeight; row++) {
+        let tr = document.createElement('tr');
+        tr.id = 'row-' + row;
+        pixelTable.appendChild(tr);
+        for (let col = 0; col < pixelWidth; col++) {
+            let td = document.createElement('td');
+            td.id = 'row-' + row + '_cell-' + col;
+            pixelTable.lastChild.appendChild(td);
+        }
+    }
+
+    // Create new instances of the all document.querySelectorAll("td").
+    let tableCells = Array.from(document.querySelectorAll("td"));
+    // Each cell should have an event listener that sets the background color of the cell to the selected color.
+    tableCells.forEach(function (cell) {
+        cell.addEventListener("click", function () {
+            this.style.backgroundColor = document.querySelector("#colorPicker").value;
+        }, false);
+    });
+}
